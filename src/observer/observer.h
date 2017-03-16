6e7c369c6a907893431873ab12458ef876ebfdfd
Ryan <ryanorz@126.com>
2017-03-16 17:16:59 +0800

    new feature: observer

---
+++ src/observer/observer.h
@@ -0,0 +1,14 @@
+#ifndef design_patterns_observer_observer_h
+#define design_patterns_observer_observer_h
+
+#include "subject.h"
+
+class Observer {
+public:
+        virtual ~Observer() {};
+        virtual void Update() {}
+protected:
+        Observer() {};
+};
+
+#endif
---
+++ test/observer/client_test.cc
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "observer/client.h"
+
+TEST_CASE("observer", "[client]") {
+        client();
+}
